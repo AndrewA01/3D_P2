50de58573c2ea52576ccb3a4fa3c5240c402ae40
AndrewA01 <a.alfaro.ar@gmail.com>
2023-08-16 14:38:31 -0700

    Notes to MWS script

--- Assets/Scripts/MWS.cs
+++ Assets/Scripts/MWS.cs
@@ -5,24 +5,27 @@ using UnityEngine;
 public class MWS : MonoBehaviour
 
 {
+    // Reference to tires
     [SerializeField] WheelCollider FL;
     [SerializeField] WheelCollider FR;
     [SerializeField] WheelCollider RL;
     [SerializeField] WheelCollider RR;
 
-
+    // public float values
     public float acceleration = 500f;
     public float breakingForce = 250f;
     public float maxTurnAngle = 15f;
 
+    // private float values
     private float currentAcceleration = 0f;
     private float currentBreakForce = 0f;
     private float currentTurnAngle = 0f;
+
     private void FixedUpdate()
     {
-
         currentAcceleration = acceleration * Input.GetAxis("Vertical");
 
+        // Apply Braking force with Input.
         if (Input.GetKey(KeyCode.Space))
             currentBreakForce = breakingForce;
         else 
@@ -40,21 +43,21 @@ public class MWS : MonoBehaviour
         // else 
         //     currentBreakForce = 0f;
 
+
+        // AWD torque
         FL.motorTorque = currentAcceleration;
         FR.motorTorque = currentAcceleration;
         RL.motorTorque = currentAcceleration;
         RR.motorTorque = currentAcceleration;
-
+        // Braking
         FL.brakeTorque = currentBreakForce;
         FR.brakeTorque = currentBreakForce;
         RL.brakeTorque = currentBreakForce;
         RL.brakeTorque = currentBreakForce;
 
+        // Turning FL and FR tire
         currentTurnAngle = maxTurnAngle * Input.GetAxis("Horizontal");
         FL.steerAngle = currentTurnAngle;
         FR.steerAngle = currentTurnAngle;
-
-
     }
-
 }
